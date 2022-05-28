e6c82d7678f96cdf93d8b73bd0505dbbaecfb850
Swapnil Santosh Gite <gitesmait@gmail.com>
2022-05-28 13:52:19 +0530

    first commit

---
+++ app/src/main/java/com/example/collegeapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.collegeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private CardView cardViewNotice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cardViewNotice = (CardView) findViewById(R.id.addNotice);
+
+        cardViewNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this,UploadNotice.class);
+        startActivity(i);
+    }
+}
