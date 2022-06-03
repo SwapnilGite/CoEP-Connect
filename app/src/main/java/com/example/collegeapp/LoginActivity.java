3f83301f94e38dd6b87807195e43bb94247a68c0
Swapnil Santosh Gite <gitesmait@gmail.com>
2022-06-03 18:20:07 +0530

    admin login page and logout button

--- app/src/main/java/com/example/collegeapp/LoginActivity.java
+++ app/src/main/java/com/example/collegeapp/LoginActivity.java
@@ -2,13 +2,95 @@ package com.example.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText userEmail,userPass;
+    private TextView tvShow;
+    private RelativeLayout loginBtn;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
+    private String email,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        sharedPreferences = this.getSharedPreferences("login",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        userEmail = findViewById(R.id.user_email);
+        userPass = findViewById(R.id.user_password);
+        tvShow = findViewById(R.id.txt_show);
+        loginBtn = findViewById(R.id.Login_btn);
+
+        if(sharedPreferences.getString("isLogin","false").equals("yes"))
+        {
+            openDash();
+        }
+        tvShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userPass.getInputType() == 144) //if pass is in show mode then hide it
+                {
+                    userPass.setInputType(129); //hide
+                    tvShow.setText("Show");
+
+                }
+                else
+                {
+                    userPass.setInputType(144); //show
+                    tvShow.setText("Hide");
+                }
+                userPass.setSelection(userPass.getText().length());
+            }
+
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateDate();   //validate data when login button is pressed
+            }
+        });
+    }
+
+    private void validateDate() {
+        email = userEmail.getText().toString();
+        pass = userPass.getText().toString();
+
+        if(email.isEmpty())
+        {
+            userEmail.setError("Required");
+            userEmail.requestFocus();
+        }
+        else if(pass.isEmpty())
+        {
+            userPass.setError("Required");
+            userPass.requestFocus();
+        }
+        else if(email.equals("admin@gmail.com") && pass.equals("12345"))
+        {
+            editor.putString("isLogin","yes");
+            editor.commit();
+            openDash();
+        }
+        else {
+            Toast.makeText(this,"Invalid Credentials",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void openDash() {
+
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        finish();
+
     }
 }
--- app/src/main/java/com/example/collegeapp/MainActivity.java
+++ app/src/main/java/com/example/collegeapp/MainActivity.java
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private CardView cardViewNotice;
+    private CardView logout;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cardViewNotice = (CardView) findViewById(R.id.addNotice);
-
         cardViewNotice.setOnClickListener(this);
+        logout = (CardView)findViewById(R.id.Logout_btn);
+        logout.setOnClickListener(this);
+        sharedPreferences = this.getSharedPreferences("login",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        if(sharedPreferences.getString("isLogin","false").equals("false"))
+        {
+            openLogin();
+        }
+    }
+
+    private void openLogin() {
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        finish();
     }
 
     @Override
@@ -27,6 +44,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             i = new Intent(this, UploadNotice.class);
             startActivity(i);
             break;
+            case R.id.Logout_btn:
+
+                editor.putString("isLogin","false");
+                editor.commit();
+                Intent i2;
+                i2 = new Intent(this,LoginActivity.class);
+                startActivity(i2);
+                break;
         }
     }
 }
